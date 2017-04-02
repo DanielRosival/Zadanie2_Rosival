f8333edec0be4a5f53831b340a05f03575e21f93
DanielRosival <daniel.rosival@gmail.com>
2017-04-02 17:37:20 +0200

    zadanie dlzky

--- Zadanie2_Rosival.c
+++ Zadanie2_Rosival.c
@@ -2,6 +2,11 @@
 #include <math.h>
 #define N 36
 
+void destroy_string(char *to_destroy){
+    free(to_destroy);
+}
+
+
 void nahodny_rastuci_retazec(char *r, char l){
 	unsigned char aux_array[N];
 
@@ -15,9 +20,30 @@ void nahodny_rastuci_retazec(char *r, char l){
 		aux_array[i+10] = 'a'+i;
 		printf("%c ", aux_array[i+10]);
  	}
+
+ 	r[l]
+
 }
 
 
 
 int main(int argc, char const *argv[]){
+	char l;
+	char *r;
+
+	printf("Zadajte dlzku retazca,min. 1 a max. dlzka 35\n");
+	scanf("%c",&l);
+
+	while ((l<1)||(l>N))
+	{
+		printf("Zadali ste nespravnu dlzku retazca, zadajte znova prosim.\n");
+		scanf("%c",&l);
+	}
+
+	r=(char *) malloc(l);
+
+	destroy_string(r);
+
+
+
 }
