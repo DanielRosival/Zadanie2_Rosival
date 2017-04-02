559e58391d2e29d6cac3b25bbd267b44b646350d
DanielRosival <daniel.rosival@gmail.com>
2017-04-02 16:56:08 +0200

    modified .gitignored, start of the function

--- Zadanie2_Rosival.c
+++ Zadanie2_Rosival.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+#define N 36
 
-void zadanie(){
-	
+void nahodny_rastuci_retazec(char *r, char l){
+	unsigned char aux_array[N];
+
+	for (int i = 0; i < 10; i++)
+	{
+		aux_array[i] = '0'+i;
+ 		printf("%c ", aux_array[i]);
+ 	}
+ 	for (int i = 0; i < 26; i++)
+	{
+		aux_array[i+10] = 'a'+i;
+		printf("%c ", aux_array[i+10]);
+ 	}
 }
 
-int main(int argc, char const *argv[]){
-	printf("Hello World!\n");
-	printf("Test\n");
-	printf("Change3\n");
-	printf("Change4\n");
-	return 0;
 
+
+int main(int argc, char const *argv[]){
 }
